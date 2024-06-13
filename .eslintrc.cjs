69058baf8b5a62fd86a646f45c46c1ecc1ab7b11
Wang-Yi-Hao <1767082837@qq.com>
2024-06-13 16:59:33 +0800

    element+

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,11 +3,7 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-prettier/skip-formatting'],
   parserOptions: {
     ecmaVersion: 'latest'
   },
@@ -17,7 +13,7 @@ module.exports = {
       {
         singleQuote: true, // 单引号
         semi: false, // 无分号
-        printWidth: 80, // 每行宽度至多80字符
+        printWidth: 150, // 每行宽度至多150字符
         trailingComma: 'none', // 不加对象|数组最后逗号
         endOfLine: 'auto' // 换行符号不限制（win mac 不一致）
       }
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
-<script setup></script>
+<script setup>
+import { useRoute, useRouter } from 'vue-router'
+const router = useRouter()
+const route = useRoute()
+
+const goHome = () => {
+  router.push('Home')
+  console.log(route, router)
+}
+const goList = () => {
+  router.push('List')
+}
+</script>
 
 <template>
   <header>app</header>
+  <button @click="goHome">首页</button>
+  <button @click="goList">list</button>
+  <el-button type="primary">Primary</el-button>
+  <el-button type="success">Success</el-button>
+  <el-button type="info">Info</el-button>
 </template>
 
 <style scoped></style>
