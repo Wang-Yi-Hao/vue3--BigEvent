715d0921b865e36c0dc60bde4387126a2eb66141
Wang-Yi-Hao <1767082837@qq.com>
2024-06-14 16:48:18 +0800

    前期配置

--- src/App.vue
+++ src/App.vue
@@ -10,15 +10,25 @@ const goHome = () => {
 const goList = () => {
   router.push('List')
 }
+
+import { useUserStore } from './stores'
+const userStore = useUserStore()
 </script>
 
 <template>
+  <router-view></router-view>
+  <hr />
   <header>app</header>
   <button @click="goHome">首页</button>
   <button @click="goList">list</button>
   <el-button type="primary">Primary</el-button>
   <el-button type="success">Success</el-button>
   <el-button type="info">Info</el-button>
+
+  <p>{{ userStore.token }}</p>
+
+  <el-button type="primary" @click="userStore.setToken('1561564654')">登录</el-button>
+  <el-button type="success" @click="userStore.setToken()">退出</el-button>
 </template>
 
 <style scoped></style>
