7912b7fa928962b80065b9ec4c81cc25b51a4244
Wang-Yi-Hao <1767082837@qq.com>
2024-06-24 12:46:53 +0800

    login/注册

--- src/views/login/LoginPage.vue
+++ src/views/login/LoginPage.vue
@@ -1,1 +1,119 @@
-<template>登录页</template>
+<script setup>
+import { User, Lock } from '@element-plus/icons-vue'
+import { ref } from 'vue'
+const isRegister = ref(true)
+
+// 注册
+const formModel = ref({
+  username: '',
+  password: '',
+  repassword: ''
+})
+const rules = {
+  username: [
+    { required: true, message: '请输入用户名', trigger: 'blur' },
+    { min: 3, max: 9, message: '用户名长度在3-5位', trigger: 'change' }
+  ],
+  password: [
+    { required: true, message: '请输入密码', trigger: 'blur' },
+    { min: 9, max: 15, message: '密码长度在9-15位', trigger: 'change' },
+    { pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]/, message: '密码至少包含一个字母和一个数字', trigger: 'blur' }
+  ],
+  repassword: [
+    { required: true, message: '请输入密码', trigger: 'blur' },
+    { min: 9, max: 15, message: '密码长度在9-15位', trigger: 'change' },
+    { pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]/, message: '密码至少包含一个字母和一个数字', trigger: 'blur' },
+    {
+      validator: (rule, value, callback) => {
+        if (value !== formModel.value.password) {
+          callback(new Error('两次密码输入不一致'))
+        } else {
+          callback()
+        }
+      },
+      trigger: 'blur'
+    }
+  ]
+}
+</script>
+
+<template>
+  <el-row class="login-page">
+    <el-col :span="12" class="bg"></el-col>
+    <el-col :span="6" :offset="3" class="form">
+      <el-form ref="form" size="large" autocomplete="off" v-if="isRegister" :model="formModel" :rules="rules">
+        <el-form-item>
+          <h1>注册</h1>
+        </el-form-item>
+        <el-form-item prop="username">
+          <el-input :prefix-icon="User" placeholder="请输入用户名" v-model="formModel.username"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input :prefix-icon="Lock" type="password" placeholder="请输入密码" v-model="formModel.password"></el-input>
+        </el-form-item>
+        <el-form-item prop="repassword">
+          <el-input :prefix-icon="Lock" type="password" placeholder="请输入再次密码" v-model="formModel.repassword"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="button" type="primary" auto-insert-space> 注册 </el-button>
+        </el-form-item>
+        <el-form-item class="flex">
+          <el-link type="info" :underline="false" @click="isRegister = false"> ← 返回 </el-link>
+        </el-form-item>
+      </el-form>
+      <el-form ref="form" size="large" autocomplete="off" v-else>
+        <el-form-item>
+          <h1>登录</h1>
+        </el-form-item>
+        <el-form-item>
+          <el-input :prefix-icon="User" placeholder="请输入用户名"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input name="password" :prefix-icon="Lock" type="password" placeholder="请输入密码"></el-input>
+        </el-form-item>
+        <el-form-item class="flex">
+          <div class="flex">
+            <el-checkbox>记住我</el-checkbox>
+            <el-link type="primary" :underline="false">忘记密码？</el-link>
+          </div>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="button" type="primary" auto-insert-space>登录</el-button>
+        </el-form-item>
+        <el-form-item class="flex">
+          <el-link type="info" :underline="false" @click="isRegister = true"> 注册 → </el-link>
+        </el-form-item>
+      </el-form>
+    </el-col>
+  </el-row>
+</template>
+
+<style lang="scss" scoped>
+.login-page {
+  height: 100vh;
+  background-color: #fff;
+  .bg {
+    background:
+      url('@/assets/logo2.png') no-repeat 60% center / 240px auto,
+      url('@/assets/login_bg.jpg') no-repeat center / cover;
+    border-radius: 0 20px 20px 0;
+  }
+  .form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    user-select: none;
+    .title {
+      margin: 0 auto;
+    }
+    .button {
+      width: 100%;
+    }
+    .flex {
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+    }
+  }
+}
+</style>
